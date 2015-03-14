aacdda3bcd70999dbb80a52d23286aa0473e6a4d
root <root@MSTLEONARDGTEST.(none)>
2015-03-14 19:50:59 +0530

    Initial

---
+++ app/controller.py
@@ -0,0 +1,310 @@
+from flask import render_template, request,flash, redirect, url_for
+from app import app, db
+from app.models import Post,Comment,Users, Terms
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.login import LoginManager, login_user,UserMixin, logout_user, login_required
+
+
+#Main
+@app.route('/' )
+def index():
+  post = Post.query.all()    
+  return render_template('index.html', post=post)
+
+
+#End Main Content
+#Posts
+@app.route('/post/' )
+@login_required
+def post_index():
+  post = Post.query.all() 
+  terms =Terms.query.all()  
+  return render_template('/post/index.html', post=post, terms=terms)
+
+@app.route('/post/add' , methods=['POST', 'GET'])
+@login_required
+def post_add():
+    terms=Terms.query.all()
+    if request.method == 'POST':
+        post=Post(request.form['author'],request.form['title'],request.form['content'], request.form['published'],request.form['category'])
+        post_add=post.add(post)
+        if not post_add:
+                   flash("Add was successful")           
+                   return redirect(url_for('post_index')) 
+        else:
+                   flash("Oops something went wrong")        
+        
+    return render_template('/post/add.html', terms=terms)
+	
+@app.route('/post/update/<id>' , methods=['POST', 'GET'])
+@login_required
+def post_update (id):
+    #Getting user by primary key:
+    post = Post.query.get(id)
+    error=query_error(post, post_index)
+    if not error:
+        terms=Terms.query.all()
+        if request.method == 'POST':
+            post.author=request.form['author']
+            post.title = request.form['title']
+            post.content =  request.form['content']
+            post.published=request.form['published']
+            post.category=request.form['category']
+            post_update=post.update()
+            if not post_update:
+                   flash("Update was successful")           
+                   return redirect(url_for('post_index')) 
+                   
+            else:
+                   flash("Oops something went wrong")
+            
+        return render_template('post/update.html', post=post, terms=terms)
+    else:
+          return error
+	
+
+@app.route('/delete/<id>' , methods=['POST', 'GET'])
+@login_required
+def post_delete (id):
+     post = Post.query.get(id)
+     error = query_error(post, post_index)
+     if not error:            
+            post.delete(post)
+            flash("Post was deleted successfully")
+            return redirect(url_for('post_index'))
+     else:
+          return error
+	   
+     
+     
+     
+ #Comments
+@app.route('/comment/' )
+@login_required
+def comment_index():
+  comments = Comment.query.all()    
+  return render_template('/comment/index.html', comments=comments)
+  
+@app.route('/comment/add/<post_id>', methods=['POST', 'GET'])
+@login_required
+def comment_add(post_id):
+    post = Post.query.get(post_id)
+    error = query_error(post,comment_index)
+    if not error:
+        if request.method == 'POST':
+           comment=Comment(request.form['author'], request.form['website'], request.form['content'],post_id,request.form['approved'])
+           comment_add=comment.add(comment)
+           if not comment_add:
+               flash("Add was successful")           
+               return redirect(url_for('comment_index')) 
+               
+           else:
+               flash("Oops something went wrong")
+               
+                        
+        return render_template('comment/add.html', post_id=post_id)
+    else:
+        return error
+    
+@app.route('/comment/update/<id>', methods=['POST', 'GET'])
+@login_required
+def comment_update (id):
+    comment = Comment.query.get(id)
+    error = query_error(comment,comment_index)
+    if not error:
+        if request.method == 'POST':
+            comment.author=request.form['author']
+            comment.website=request.form['website']
+            comment.content=request.form['content']
+            comment.approved=request.form['approved']
+            comment_update=comment.update()
+            if comment_update == 'false':
+               flash("Oops something went wrong")
+            else:
+               flash("Update was successful")           
+               return redirect(url_for('comment_index'))        
+        return render_template('comment/update.html', comment=comment)
+    else:
+        return error
+  
+    
+@app.route('/comment/delete/<id>', methods=['POST', 'GET'])
+@login_required
+def comment_delete (id):
+    comment = Comment.query.get(id)
+    error = query_error(comment,comment_index)
+    if not error:
+            
+            comment.delete(comment)
+            flash("Comment was deleted successfully")
+            return redirect(url_for('comment_index'))
+    else:
+          return error
+
+
+#Users
+@app.route('/user/' )
+@login_required
+def user_index():
+  users = Users.query.all()    
+  return render_template('/user/index.html', users=users)
+
+@app.route('/user/add' , methods=['POST', 'GET'])
+@login_required
+def user_add():
+    if request.method == 'POST':
+        name=request.form['name']
+        email=request.form['email']
+        password=generate_password_hash(request.form['password'])
+        user=Users(email,password,name)
+        user_add=user.add(user)
+        db_commit(user_add,user_index)
+       
+        return redirect(url_for('user_index'))
+                
+        
+    return render_template('/user/add.html')
+	
+@app.route('/user/update/<id>' , methods=['POST', 'GET'])
+@login_required
+def user_update (id):
+    #Getting user by primary key:
+    user = Users.query.get(id)
+    error=query_error(user, user_index)
+    if not error:
+        if request.method == 'POST':
+            user.email=request.form['email']
+            user.password = request.form['password']
+            user.name =  request.form['name']
+            user_update=user.update()
+            db_commit(user_update, user_index)
+            
+        return render_template('user/update.html', user=user)
+    else:
+          return error
+	
+
+@app.route('/user/delete/<id>' , methods=['POST', 'GET'])
+@login_required
+def user_delete (id):
+     user = Users.query.get(id)
+     error = query_error(user,user_index)
+     if not error:            
+            user_delete=user.delete(user)
+            #if does not return an error
+            db_commit(user_delete, user_index)
+            return redirect(url_for('user_index'))
+     else:
+          return error
+          
+          
+#START TERMS
+@app.route('/terms/' )
+@login_required
+def term_index():
+  terms = Terms.query.all()    
+  return render_template('/terms/index.html', terms=terms)
+
+@app.route('/terms/add' , methods=['POST', 'GET'])
+@login_required
+def term_add():
+     if request.method == 'POST':
+        name=request.form['name']
+        description=request.form['description']
+        term=Terms(name,description)
+        term_add=term.add(term)
+        db_commit(term_add,term_index)
+       
+        return redirect(url_for('term_index'))
+                
+        
+     return render_template('/terms/add.html')
+
+@app.route('/terms/update/<id>' , methods=['POST', 'GET'])
+@login_required
+def term_update (id):
+    #Getting user by primary key:
+    term = Terms.query.get(id)
+    error=query_error(term, term_index)
+    if not error:
+        if request.method == 'POST':
+            term.name=request.form['name']
+            term.description = request.form['description']            
+            term_update=term.update()
+            db_commit(term_update, term_index)
+            
+        return render_template('terms/update.html', term=term)
+    else:
+          return error
+	
+
+	
+
+@app.route('/terms/delete/<id>' , methods=['POST', 'GET'])
+@login_required
+def term_delete (id):
+     term = Terms.query.get(id)
+     error = query_error(term,term_index)
+     if not error:            
+            term_delete=term.delete(term)
+            #if does not return an error
+            db_commit(term_delete, term_index)
+            return redirect(url_for('term_index'))
+     else:
+          return error
+          
+          
+#END TERMS
+      
+#Initialize the LoginManager from Flask-Login		
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+	
+@app.route('/login', methods=['POST', 'GET'])
+def login ():
+  if request.method == 'POST':
+    email=request.form['email']
+    password=request.form['password']
+    user=Users.query.filter_by(email=email).first()
+    error=query_error(user,login)
+    if not error:
+        if check_password_hash(user.password,password):
+          login_user(user)
+          return redirect(url_for('post_index'))
+        else:
+         flash("invalid username/password")
+    else:
+        return error
+  return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+
+#End Login Manager
+    
+#Errors
+def db_commit(data,func):
+    #if does not return an error
+    if not data:
+       flash("Great Success") 
+       return redirect(url_for(str(func.__name__))) 
+    else:
+       message=data
+       flash(message)
+     
+def query_error(var,func):
+  if var == None:
+      flash("This entry does not exist in the database")
+      return redirect(url_for(str(func.__name__)))
+     
+
+
+
---
+++ app/models.py
@@ -0,0 +1,127 @@
+from app import db
+from flask.ext.login import LoginManager, login_user,UserMixin, logout_user
+from sqlalchemy.sql.expression import text
+from sqlalchemy.exc import SQLAlchemyError
+
+      
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  author = db.Column(db.String(255))
+  title = db.Column(db.String(255),nullable=False)
+  created_on=db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp())
+  content = db.Column(db.Text)
+  published = db.Column(db.Boolean, default=1, nullable=False)
+  #add ondelete manually in alembic
+  category = db.Column(db.Integer, db.ForeignKey('terms.id', ondelete="CASCADE"))
+  comments = db.relationship('Comment', backref='comment', lazy='dynamic')
+  
+  def __init__(self, author,title, content,published,category):
+        self.title = title
+        self.author = author
+        self.content=content
+        self.published=published
+        self.category=category
+        
+  def get_id(self):
+    return str(self.id)
+    
+  def add(self,post):
+      db.session.add(post)
+      return session_commit()        
+        
+  def update(self):     
+      return session_commit()
+      
+  def delete(self,post):
+     db.session.delete(post)
+     return session_commit()
+
+class Comment(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  author = db.Column(db.String(128),nullable=False)
+  created_on = db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp(),nullable=False)
+  website = db.Column(db.String(255))
+  content = db.Column(db.Text, nullable=False)
+  approved = db.Column(db.Boolean, default=1)
+  post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="CASCADE"))
+  
+  def __init__(self, author,website, content,post_id,approved):
+        self.author = author
+        self.website = website
+        self.content=content
+        self.approved=approved
+        self.post_id=post_id  
+                
+  def get_id(self):
+    return unicode(self.id)
+    
+  def add(self, comment):
+      db.session.add(comment)
+      return session_commit()
+        
+  def update(self):            
+      return session_commit()
+      
+  def delete(self,comment):
+     db.session.delete(comment)
+     return session_commit()
+   
+        
+class Users(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(250), unique=True, nullable=False)
+  password = db.Column(db.String(250), nullable=False)
+  name = db.Column(db.String(250), nullable=False)
+  created_on = db.Column(db.TIMESTAMP,server_default=db.func.current_timestamp(),nullable=False)
+ 
+  
+  def __init__(self,email,password,name):
+    self.email=email
+    self.password=password
+    self.name=name
+
+  def get_id(self):
+    return str(self.id)
+    
+  def add(self,user):
+     db.session.add(user)   
+     return session_commit ()       
+        
+  def update(self):     
+      return session_commit()
+      
+  def delete(self,user):
+     db.session.delete(user)
+     return session_commit()
+     
+class Terms(db.Model):
+     id=db.Column(db.Integer, primary_key=True)
+     name=db.Column(db.String, unique=True, nullable=False)
+     description=db.Column(db.String)
+     group=db.Column(db.String, nullable=False, server_default='category')
+     
+     def __init__(self,name,description):
+        self.name=name
+        self.description=description
+    
+     def add(self,term):
+         db.session.add(term)   
+         return session_commit()       
+        
+     def update(self):     
+      return session_commit()
+      
+     def delete(self,term):
+         db.session.delete(term)
+         return session_commit()
+
+
+ 
+#Universal functions 
+
+def  session_commit ():
+      try:        
+        db.session.commit()        
+      except SQLAlchemyError as message:
+        db.session.rollback()
+        return message[0]
