238d52fcb8ca43d500b86619277a4ec598e7ee41
Leo G <leo@leog.in>
2016-07-12 16:56:14 -0400

    db config

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # DATABASE SETTINGS
-pg_db_username = 'postgres'
-pg_db_password = ''
-pg_db_name = 'freddy'
-pg_db_hostname = 'localhost'
+pg_db_username = 'nuxkdhzxuvlkbx'
+pg_db_password = 'GVSy1_tzaEkHqxQS0_hMeLL-J9'
+pg_db_name = 'djdbnrpjo5lin'
+pg_db_hostname = 'ec2-54-243-249-159.compute-1.amazonaws.com'
 
 # MYSQL
 mysql_db_username = 'root'
